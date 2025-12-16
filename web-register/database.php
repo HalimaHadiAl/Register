41c65b587fd750b96514327b63b71b9bd3efbf29
Halima Hadi Alfina <finalimdina280306@gmail.com>
2025-12-16 00:00:45 -0800

    Delete web-register directory

--- web-register/database.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "", "ika_polines");
-
-if (!$conn) {
-    die("Koneksi database gagal!");
-}
--- web-register/layout/footer.php
+++
@@ -1,9 +0,0 @@
-</div>
-
-<footer class="text-center py-4 text-muted">
-    © <?= date('Y') ?> Ikapolines
-</footer>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
